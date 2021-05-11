c80c937b95107d059ea09deec134ffaed5ec27cc
Phạm Thành Nam <64145033+zvw180@users.noreply.github.com>
2021-05-11 19:25:15 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-$conn =new mysqli("localhost","root","ptn141122","pham_ptn_shop");
+$conn =new mysqli("localhost","root","","pham_ptn_shop");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
